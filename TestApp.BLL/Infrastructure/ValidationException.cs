a7fdfd32b4eff08388581f2dd5b52b5b7d022340
Maksim_Kazlou1 <Maksim_Kazlou1@epam.com>
2018-03-05 10:08:57 +0300

    Correct BLL was added

---
+++ TestApp.BLL/Infrastructure/ValidationException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestApp.BLL.Infrastructure
+{
+    public class ValidationException : Exception
+    {
+        public IDictionary<string, string> ValidationErrors { get; protected set; }
+        public ValidationException(IDictionary<string, string> errors)
+        {
+            ValidationErrors = errors;
+        }
+        public override string Message
+        {
+            get
+            {
+                var messages = ValidationErrors.Select(dp => $"Field: {dp.Key}  Error: {dp.Value}").ToList();
+                return string.Join(";", messages);
+            }
+        }
+    }
+}
---
+++ TestApp.BLL/Utils/AutofacRegistrator.cs
@@ -0,0 +1,14 @@
+﻿using TestApp.DAL.Interfaces;
+using TestApp.DAL.DataAccess;
+using Autofac;
+
+namespace TestApp.BLL.Utils
+{
+    public static class AutofacRegistrator
+    {
+        public static void RegisterDependencies(ContainerBuilder builder, string connectionName)
+        {
+            builder.RegisterType<EntityFrameworkUnitOfWork>().As<IUnitOfWork>().WithParameter("connectionName", connectionName);
+        }
+    }
+}
