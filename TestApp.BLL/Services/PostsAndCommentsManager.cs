f74bdd6768c277d0ff13633244aeda66eeedf541
Maksim_Kazlou1 <Maksim_Kazlou1@epam.com>
2018-03-05 11:01:59 +0300

    Methods of manager were documented

--- TestApp.BLL/Services/PostsAndCommentsManager.cs
+++ TestApp.BLL/Services/PostsAndCommentsManager.cs
@@ -43,6 +43,12 @@ namespace TestApp.BLL.Services
             });
         }
 
+        /// <summary>
+        /// Adding new comment
+        /// </summary>
+        /// <param name="commentDTO">comment to be added</param>
+        /// <exception cref="ValidationException">Thrown when comment validation don't pass</exception>
+        /// <exception cref="DatabaseException">Thrown when problems on data source level appear</exception>
         public void AddComment(CommentDTO commentDTO)
         {
             var validationErrors = _validator.GetCommentDTOValidationErrors(commentDTO);
@@ -69,6 +75,12 @@ namespace TestApp.BLL.Services
             }
         }
 
+        /// <summary>
+        /// Adding new post
+        /// </summary>
+        /// <param name="postDTO">post to be added</param>
+        /// <exception cref="ValidationException">Thrown when post validation don't pass</exception>
+        /// <exception cref="DataAccessException">Thrown when problems on data source level appear</exception>
         public void AddPost(PostDTO postDTO)
         {
             var validationErrors = _validator.GetPostDTOValidationErrors(postDTO);
@@ -91,6 +103,11 @@ namespace TestApp.BLL.Services
             }
         }
 
+        /// <summary>
+        /// Deleting comment by its ID
+        /// </summary>
+        /// <param name="id">ID of comment to be deleted</param>
+        /// <exception cref="DataAccessException">Thrown when problems on data source level appear</exception>
         public void DeleteCommentById(int id)
         {
             _db.Comments.Delete(id);
@@ -105,6 +122,11 @@ namespace TestApp.BLL.Services
             }
         }
 
+        /// <summary>
+        /// Deleting post by its ID
+        /// </summary>
+        /// <param name="id">ID of post to be deleted</param>
+        /// <exception cref="DataAccessException">Thrown when problems on data source level appear</exception>
         public void DeletePostById(int id)
         {
             _db.Posts.Delete(id);
@@ -119,30 +141,55 @@ namespace TestApp.BLL.Services
             }
         }
 
+        /// <summary>
+        /// Getting all existing posts asynchronously
+        /// </summary>
+        /// <returns>posts as IEnumerable object</returns>
         public async Task<IEnumerable<PostDTO>> GetAllPostsAsync()
         {
             var posts = await _db.Posts.GetAllAsync();
             return Mapper.Map<IEnumerable<Post>, IEnumerable<PostDTO>>(posts);
         }
 
+        /// <summary>
+        /// Getting all comments related to specific post
+        /// </summary>
+        /// <param name="postId">ID of post</param>
+        /// <returns>comments as IEnumerable object</returns>
         public async Task<IEnumerable<CommentDTO>> GetCommentsOfPostAsync(int postId)
         {
             var comments = await _db.Comments.FindAsync(c => c.PostId == postId);
             return Mapper.Map<IEnumerable<Comment>, IEnumerable<CommentDTO>>(comments);
         }
 
+        /// <summary>
+        /// Getting comment by its ID
+        /// </summary>
+        /// <param name="id">ID of comment</param>
+        /// <returns>comment with requested ID, null if doesn't exist</returns>
         public CommentDTO GetCommentById(int id)
         {
             var comment = _db.Comments.Get(id);
             return Mapper.Map<CommentDTO>(comment);
         }
 
+        /// <summary>
+        /// Getting post by its ID
+        /// </summary>
+        /// <param name="id">ID of post</param>
+        /// <returns>post with requested ID, null if doesn't exist</returns>
         public PostDTO GetPostById(int id)
         {
             var post = _db.Posts.Get(id);
             return Mapper.Map<PostDTO>(post);
         }
 
+        /// <summary>
+        /// Updating comment asynchronously
+        /// </summary>
+        /// <param name="commentDTO">comment to be updated</param>
+        /// <exception cref="ValidationException">Thrown when comment validation don't pass</exception>
+        /// <exception cref="DataAccessException">Thrown when problems on data source level appear</exception>
         public async Task UpdateCommentAsync(CommentDTO commentDTO)
         {
             var validationErrors = _validator.GetCommentDTOValidationErrors(commentDTO);
@@ -168,6 +215,12 @@ namespace TestApp.BLL.Services
             }
         }
 
+        /// <summary>
+        /// Updating post asynchronously
+        /// </summary>
+        /// <param name="postDTO">post to be updated</param>
+        /// <exception cref="ValidationException">Thrown when post validation don't pass</exception>
+        /// <exception cref="DataAccessException">Thrown when problems on data source level appear</exception>
         public async Task UpdatePostAsync(PostDTO postDTO)
         {
             var validationErrors = _validator.GetPostDTOValidationErrors(postDTO);
